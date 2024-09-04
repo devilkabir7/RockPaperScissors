31a2305358535b11020448f8935105af116e922e
Kabir <kkalra200@gmail.com>
2024-09-04 10:31:44 +0530

    Add computer choice generation

--- game.js
+++ game.js
@@ -1,3 +1,21 @@
 "use strict"
 
-console.log("Hello world")
+function genComputerChoice() {
+
+    let currencChoice = Math.round(Math.random() * 10) % 3;
+
+    switch(currencChoice) {
+ 
+        case 0: 
+            return "Rock";
+            break;
+        case 1:
+            return "Paper";
+            break;
+        case 2:
+            return "Scissors";
+            break;
+    }
+}
+
+console.log(genComputerChoice());
